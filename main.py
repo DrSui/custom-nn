bf24667a5170fe5906d2a73cc28ec61f82d62411
Leo Savinov <121363379+DrSui@users.noreply.github.com>
2025-02-16 22:44:00 +0000

    workinh but not trained

--- main.py
+++ main.py
@@ -1,138 +1,162 @@
 import numpy as np
+import cupy as cp
 from data import get_data
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import time
 from scipy import signal
-from scipy import special
 from resize import ResizeIMG_64_64
-from PIL import Image
 #math funcitons
 #activation funcitons
 def fast_gelu(x):
-    return 1 / (1 + np.exp(-x))
+    return 1 / (1 + cp.exp(-x))
 
 def delta_fast_gelu(x):
     return (x * (1 - x))
-
+def softmax(x):
+    relative_x = cp.exp(x-cp.max(x))
+    return relative_x / relative_x.sum()
 class NeuralNetwork:
     def __init__(self, n, learn_rate, size, kernel_size):
         #set hyper parameters
         self.array = ["Bread", "Dairy product", "Dessert", "Egg", "Fried food", "Meat", "Noodles-Pasta", "Rice", "Seafood", "Soup", "Vegetable-Fruit"]
         self.n = n
         self.learn_rate = learn_rate
-        self.kernel_filter = np.random.uniform(-0.5,0.5, (kernel_size,kernel_size,kernel_size))
+        self.kernel_filter = cp.random.uniform(-0.5,0.5, (kernel_size,kernel_size,kernel_size))
         #set weights
         entrance_size = (size-kernel_size + 1)**2
         #                                                        (62,62)
-        self.weight_input_hidden1 = np.random.uniform(-0.5, 0.5, (11532,))
-        self.weight_hidden1_hidden2 = np.random.uniform(-0.5, 0.5, (11532,))
-        self.weight_hidden2_hidden3 = np.random.uniform(-0.5, 0.5, (11532, ))
-        self.weight_hidden3_output = np.random.uniform(-0.5, 0.5, (11,11532))
+        self.weight_input_hidden1 = cp.random.uniform(-0.5, 0.5, (3844,))
+        self.weight_hidden1_hidden2 = cp.random.uniform(-0.5, 0.5, (3844,))
+        self.weight_hidden2_hidden3 = cp.random.uniform(-0.5, 0.5, (3844, ))
+        self.weight_hidden3_output = cp.random.uniform(-0.5, 0.5, (11,3844))
         print(self.weight_hidden3_output.shape)
         #set biases
-        self.bias_input_hidden1 = np.zeros((11532, ))
-        self.bias_hidden1_hidden2 = np.zeros((11532, ))
-        self.bias_hidden2_hidden3 = np.zeros((11532, ))
-        self.bias_hidden3_output = np.zeros((11, ))
-        self.bias_convolved = np.zeros((62,62,3))
+        self.bias_input_hidden1 = cp.zeros(3844, )
+        self.bias_hidden1_hidden2 = cp.zeros(3844, )
+        self.bias_hidden2_hidden3 = cp.zeros(3844, )
+        self.bias_hidden3_output = cp.zeros(11, )
+        self.bias_convolved = cp.zeros((62,62,1))
         
     def forward(self, x):
         print("here")
         print(x.shape)
         print(self.kernel_filter.shape)
-        self.convolved_unbias = signal.fftconvolve(x,self.kernel_filter,mode="valid")
+        self.convolved_unbias = cp.array(signal.fftconvolve(x.get(),self.kernel_filter.get(),mode="valid"))
         self.convolved = self.convolved_unbias + self.bias_convolved
         print(self.convolved.shape)
-        self.convolved = self.convolved.reshape(11532,)
+        self.convolved_flat = fast_gelu(self.convolved.reshape(3844,))
         # Forward propagation input -> hidden1
-        hidden1_pre = self.bias_input_hidden1 + (self.weight_input_hidden1 @ self.convolved)
+        hidden1_pre = self.bias_input_hidden1 + self.weight_input_hidden1 * self.convolved_flat
         self.hidden1 = fast_gelu(hidden1_pre)
         print(self.hidden1.shape, "HIDDEN SHAPE")
         # Forward propagation hidden1 -> hidden2
-        hidden2_pre = self.bias_hidden1_hidden2 + (self.weight_hidden1_hidden2 @ self.hidden1)
+        hidden2_pre = self.bias_hidden1_hidden2 + self.weight_hidden1_hidden2 * self.hidden1
         self.hidden2 = fast_gelu(hidden2_pre)
 
         # Forward propagation hidden2 -> hidden3
         print(self.weight_hidden2_hidden3.shape, self.hidden2.shape)
-        hidden3_pre = self.bias_hidden2_hidden3 + (self.weight_hidden2_hidden3 @ self.hidden2)
+        hidden3_pre = self.bias_hidden2_hidden3 + self.weight_hidden2_hidden3 * self.hidden2
         self.hidden3 = fast_gelu(hidden3_pre)
         print(self.weight_hidden3_output.shape, self.hidden3.shape,"GRRRRRRRRRRRR") 
         # Forward propagation hidden2 -> output
-        output_pre = self.bias_hidden3_output + (self.weight_hidden3_output @ self.hidden3)
-        self.output = special.softmax(output_pre)
+        self.output_pre = self.bias_hidden3_output + (self.weight_hidden3_output @ self.hidden3)
+        print(self.output_pre)
+        self.output = softmax(self.output_pre)
         print("HERHERKJFHSDAKFJHSADFHSADLKF2")
         print(self.output.shape)
         return self.output
-    def backward(self,img,output,label):
-            # Backpropagation output -> hidden3 (cost function derivative)
-            delta_output = output - label.reshape(11,)
-            print(delta_output.shape, output.shape,self.output.shape)
-            print("BACK PROP")
-            print(self.weight_hidden3_output.shape,delta_output.shape, self.output.T.shape)
-            self.weight_hidden3_output -= self.learn_rate * (delta_output @ self.output.T)
-            self.bias_hidden3_output -= self.learn_rate * delta_output
-
-            # Backpropagation hidden3 -> hidden2 (activation function derivative)
-            delta_hidden3 = self.weight_hidden3_output.T @ delta_output * delta_fast_gelu(self.hidden3)
-            self.weight_hidden2_hidden3 -= self.learn_rate * delta_hidden3 @ self.hidden3.T
-            self.bias_hidden2_hidden3 -= self.learn_rate * delta_hidden3
-            #
-            #MIGHT NOT NEED TO TRANSPOSE WIGHTS FOR DELTA HIDDEN
-            #
-            # Backpropagation hidden2 -> hidden1 (activation function derivative)
-            delta_hidden2 = self.weight_hidden2_hidden3 @ delta_hidden3 * delta_fast_gelu(self.hidden2)
-            self.weight_hidden1_hidden2 -= self.learn_rate * delta_hidden2 @ self.hidden2.T
-            self.bias_hidden1_hidden2 -= self.learn_rate * delta_hidden2
-
-            # Backpropagation hidden1 -> self.convolved (activation function derivative)
-            delta_hidden1 = self.weight_hidden1_hidden2 @ delta_hidden2 * delta_fast_gelu(self.hidden1)
-            print(delta_hidden1)
-            print("ASDASDASDASD")
-            self.weight_input_hidden1 -= self.learn_rate * delta_hidden1 @ self.hidden1.T
-            self.bias_input_hidden1 -= self.learn_rate * delta_hidden1
-
-            #Backpropagation convoled -> input
-            print(delta_hidden1.shape)
-            k_gradient = np.zeros(self.kernel_filter.shape)
-            k_gradient = signal.fftconvolve(img.reshape(64,64,3),delta_hidden1.T.reshape(62,62,3), "valid")
-            #bias_gradient = signal.fftconvolve(img.reshape(64,64,3),self.kernel_filter, "valid")
-            self.bias_convolved -= self.learn_rate*(self.convolved-delta_hidden1).reshape(62,62,3)
-            self.kernel_filter -= self.learn_rate * k_gradient
-            print("END OF BACK PROP")
+    
+    def backward(self, img, output, label):
+        # Convert label to one-hot encoding if it's not already
+        if label.size == 1:
+            one_hot_label = cp.zeros(11)
+            one_hot_label[int(label)] = 1
+        else:
+            one_hot_label = label
+            
+        # Backpropagation output -> hidden3 (softmax cross-entropy gradient)
+        # For softmax + cross-entropy, the gradient is simply (output - target)
+        delta_output = self.output - one_hot_label  # Shape: (11,)
+        
+        # Update weights and biases for output layer
+        # delta_output shape: (11,)
+        # self.hidden3 shape: (3844,)
+        grad_weight_hidden3_output = cp.outer(delta_output, self.hidden3)  # Shape: (11, 3844)
+        self.weight_hidden3_output -= self.learn_rate * grad_weight_hidden3_output
+        self.bias_hidden3_output -= self.learn_rate * delta_output
+        
+        # Backpropagation hidden3 -> hidden2
+        # delta_hidden = (W.T @ delta_next) * f'(z)
+        # self.weight_hidden3_output.T shape: (3844, 11)
+        # delta_output shape: (11,)
+        delta_hidden3 = (self.weight_hidden3_output.T @ delta_output) * delta_fast_gelu(self.hidden3)  # Shape: (3844,)
+        
+        # self.weight_hidden2_hidden3 shape: (3844,)
+        # delta_hidden3 shape: (3844,)
+        # self.hidden2 shape: (3844,)
+        grad_weight_hidden2_hidden3 = delta_hidden3 * self.hidden2  # Element-wise multiplication
+        self.weight_hidden2_hidden3 -= self.learn_rate * grad_weight_hidden2_hidden3
+        self.bias_hidden2_hidden3 -= self.learn_rate * delta_hidden3
+        
+        # Backpropagation hidden2 -> hidden1
+        # delta_hidden2 shape: (3844,)
+        delta_hidden2 = (self.weight_hidden2_hidden3 * delta_hidden3) * delta_fast_gelu(self.hidden2)
+        grad_weight_hidden1_hidden2 = delta_hidden2 * self.hidden1  # Element-wise multiplication
+        self.weight_hidden1_hidden2 -= self.learn_rate * grad_weight_hidden1_hidden2
+        self.bias_hidden1_hidden2 -= self.learn_rate * delta_hidden2
+        
+        # Backpropagation hidden1 -> convolved
+        delta_hidden1 = (self.weight_hidden1_hidden2 * delta_hidden2) * delta_fast_gelu(self.hidden1)
+        grad_weight_input_hidden1 = delta_hidden1 * self.convolved_flat  # Element-wise multiplication
+        self.weight_input_hidden1 -= self.learn_rate * grad_weight_input_hidden1
+        self.bias_input_hidden1 -= self.learn_rate * delta_hidden1
+        
+        # Backpropagation convolved -> input
+        # Reshape delta_hidden1 for convolution
+        delta_conv_reshaped = delta_hidden1.reshape(62, 62, 1)
+        
+        # Correlate for gradient of convolution
+        delta_kernel = cp.array(signal.correlate(
+            img.reshape(64, 64, 3).get(),
+            delta_conv_reshaped.get(),
+            mode="valid"
+        ))
+        
+        self.bias_convolved -= self.learn_rate * delta_conv_reshaped
+        self.kernel_filter -= self.learn_rate * delta_kernel
+        
     def train(self, epochs):
         images, labels = get_data()
-        images = np.asarray(images)
-        labels = np.asarray(labels)
+        images = cp.asarray(images)
+        labels = cp.asarray(labels)
         print("training...")
         total_loss = 0.0
         nr_correct = 0
         total_time = 0
-
         for epoch in range(epochs):
-            shuffled_indices = np.random.permutation(images.shape[0])
+            shuffled_indices = cp.random.permutation(images.shape[0])
             start_time = time.time()
             for idx in shuffled_indices:
                 print("NEW GEN")
                 img = images[idx]
                 label = labels[idx]
-                img.shape += (1,)
-                label.shape += (1,)
-                one_hot_label = np.zeros((11,))
+                
+                one_hot_label = cp.zeros((11,))
                 one_hot_label[label] = 1
-                #brute force change for testing purpouses
-                img1 = np.sum(ResizeIMG_64_64("./bread.jpg"),axis=2)
-                #label = np.asarray([1,0,0,0,0,0,0,0,0,0,0])
-                print(img.shape,"IMG SHAPE") 
-                print(img1.shape, "IDEAL SHAPE")
+                
                 img = img.reshape(64,64,3)
                 output = self.forward(img)
-                nr_correct += int(np.argmax(output) == np.argmax(label))
-                print(one_hot_label.shape, output.shape)
-                loss = -one_hot_label * np.log(output) + (1 - one_hot_label) * np.log(1 - output)
+                
+                nr_correct += int(cp.argmax(output) == cp.argmax(one_hot_label))
+                
+                # Cross-entropy loss for multi-class classification
+                loss = -cp.sum(one_hot_label * cp.log(output + 1e-10))
                 total_loss += loss
-            
-                self.backward(img,output,label)
+                
+                # Call backward with proper arguments
+                self.backward(img, output, one_hot_label)
+                
             print(len(shuffled_indices))
             # Show accuracy for this epoch
             end_time = time.time()
@@ -140,16 +164,14 @@ class NeuralNetwork:
             nr_avg = round((nr_correct / images.shape[0]) * 100, 2)
             avg_loss = total_loss / images.shape[0]
             print(f"Epoch {epoch+1}/{epochs}")
-            print(f"Average Loss: {np.mean(avg_loss).item():.4f}")
+            print(f"Average Loss: {avg_loss.item():.4f}")
             print(f"Accuracy: {nr_avg}%")
             print(f"time taken: {delta_time}")
             total_time += delta_time
             nr_correct = 0
-           # if nr_avg >= 40:
-               # self.learn_rate *=.1
         print(f"total time was: {total_time} average time: {total_time/epochs}")
     def save(self):
-        np.savez("neuralNetworkData.npz", 
+        cp.savez("neuralNetworkData.npz", 
             weight_input_hidden1=self.weight_input_hidden1, 
             weight_hidden1_hidden2 = self.weight_hidden1_hidden2,
             weight_hidden2_hidden3 = self.weight_hidden2_hidden3,
@@ -160,17 +182,17 @@ class NeuralNetwork:
             bias_hidden3_output= self.bias_hidden3_output)
     def load(self):
         print("loading...")
-        data = np.load("neuralNetworkData.npz")
+        data = cp.load("neuralNetworkData.npz")
         
-        self.weight_input_hidden1 = np.asarray(data["weight_input_hidden1"])
-        self.weight_hidden1_hidden2 = np.asarray(data["weight_hidden1_hidden2"])
-        self.weight_hidden2_hidden3 = np.asarray(data["weight_hidden2_hidden3"])
-        self.weight_hidden3_output = np.asarray(data["weight_hidden3_output"])
+        self.weight_input_hidden1 = cp.asarray(data["weight_input_hidden1"])
+        self.weight_hidden1_hidden2 = cp.asarray(data["weight_hidden1_hidden2"])
+        self.weight_hidden2_hidden3 = cp.asarray(data["weight_hidden2_hidden3"])
+        self.weight_hidden3_output = cp.asarray(data["weight_hidden3_output"])
         
-        self.bias_input_hidden1 = np.asarray(data["bias_input_hidden1"])
-        self.bias_hidden1_hidden2 = np.asarray(data["bias_hidden1_hidden2"])
-        self.bias_hidden2_hidden3 = np.asarray(data["bias_hidden2_hidden3"])
-        self.bias_hidden3_output = np.asarray(data["bias_hidden3_output"])
+        self.bias_input_hidden1 = cp.asarray(data["bias_input_hidden1"])
+        self.bias_hidden1_hidden2 = cp.asarray(data["bias_hidden1_hidden2"])
+        self.bias_hidden2_hidden3 = cp.asarray(data["bias_hidden2_hidden3"])
+        self.bias_hidden3_output = cp.asarray(data["bias_hidden3_output"])
 def main():
     uinput = input("load or train or both: ")
     size = 64
@@ -202,9 +224,9 @@ def main():
 
         output = neural_net.forward(img_resized)
 
-        plt.title(f"Is it {neural_net.array[np.argmax(output).item()]} :)")
-        print(np.exp(output)/np.sum(np.exp(output)))
-        print(np.argmax(output))
+        plt.title(f"Is it {neural_net.array[cp.argmax(output).item()]} :)")
+        print(cp.exp(output)/cp.sum(cp.exp(output)))
+        print(cp.argmax(output))
         plt.show()
 
 if __name__ == "__main__":
